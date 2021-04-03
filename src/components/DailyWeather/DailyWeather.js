b7dd1f1d5e07c5c7ec713ceb4db3807b83f38492
Jacqueline Tropp <jacquelinetropp@gmail.com>
2021-04-02 19:14:08 -0500

    adding days to daily

--- src/components/DailyWeather/DailyWeather.js
+++ src/components/DailyWeather/DailyWeather.js
@@ -14,17 +14,34 @@ const DailyContent = styled.div`
 background-color: var(--color-white);
 text-align:center;
 border-radius: 20px;
+padding: 1rem;
 
 `;
 
 const DailyWeather = ({data}) => {
+    let dayOfTheWeek = [];
+    function displayDate() {
+      const today = new Date();
+      const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      for (var i = 0; i < 14; i++) {
+        if (i === 0) {
+          dayOfTheWeek.push('Today');
+        } else {
+          dayOfTheWeek.push((week[(today.getDay() + i) % 7]));
+        }
+      }
+      console.log(dayOfTheWeek);
+    }
+    displayDate();
+
     console.log(data);
     return (
         <DailyWrapper>
-        {data.map((day) => {
+        {data.map((day, index) => {
+          console.log(dayOfTheWeek[index]);
             return (
               <DailyContent key={Math.floor(Math.random() * 99)}>
-              <h6>Day of the Week</h6>
+              <h6>{`${dayOfTheWeek[index]}`}</h6>
                 <img
                   src={
                     "http://openweathermap.org/img/w/" +
@@ -35,8 +52,8 @@ const DailyWeather = ({data}) => {
                   className="neg-marbottom icon"
                 />
              
-                <h3>{day.temp.max}</h3>
-                <h5>{day.temp.min}</h5>
+                <h3>{day.temp.max}<sup>o</sup></h3>
+                <h5>{day.temp.min}<sup>o</sup></h5>
               
               </DailyContent>
             );
